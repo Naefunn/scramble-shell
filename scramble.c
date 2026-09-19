d83a500ecbf13d737b45e75ff3c8fe7e658ff88e
Naefunn <nathan.d.foster1@gmail.com>
2026-09-19 20:27:33 +0100

    implemented input read and tokenisation

--- scramble.c
+++ scramble.c
@@ -2,21 +2,58 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "./lib/linenoise.h"
 
 #define PROMPT "$ "
 #define HISTORY_LENGTH 1024
+#define MAX_ARGS 1024
+
+int s_read(char *input, char **args, int max_args){
+    int i = 0;
+
+    // split the string into tokens using spaces and tabs
+    char *token = strtok(input, " \t");
+
+    // keep extracting til there are no more or args is full
+    while(token != NULL && i < (max_args - 1)){
+        args[i++] = token;
+        token = strtok(NULL, " \t");
+    }
+    args[i] = NULL;
+    return i;
+}
 
 int main(void)
 {
+    // REPL input
     if (!linenoiseHistorySetMaxLen(HISTORY_LENGTH)) {
         fprintf(stderr, "Could not set linenoise history");
         exit(1);
     }
 
     char *line;
+    char *args[MAX_ARGS];
+    
     while((line = linenoise(PROMPT)) != NULL) {
-        fprintf(stdout, "%s\n", line);
+
+        // read step
+        int args_read = s_read(line, args, MAX_ARGS);
+
+        fprintf(stdout, "read %d args\n", args_read);
+        for (int i = 0; i < args_read; i++){
+            fprintf(stdout, "arg[%d] = %s\n", i, args[i]);
+        }
+
+        // skip empty lines
+        if(args_read == 0){
+            linenoiseFree(line);
+            continue;
+        }
+
+        // TODO eval and print step
+
+
         linenoiseHistoryAdd(line);
         linenoiseFree(line);
     }
